d36656fbf8b423a86a1e98148b0b9dbc22d44fb0
maclacerda <markos.lacerda@gmail.com>
2021-07-27 13:58:13 -0300

    Fix issue on running unit test with mocked database

--- aluraflix-api/src/test/java/com/maclacerda/aluraflix/controllers/VideosControllerTest.java
+++ aluraflix-api/src/test/java/com/maclacerda/aluraflix/controllers/VideosControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEnti
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +29,7 @@ import com.maclacerda.aluraflix.models.Video;
 @AutoConfigureTestEntityManager
 @Transactional
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class VideosControllerTest {
 
 	private MockHttpServletRequestBuilder request;
